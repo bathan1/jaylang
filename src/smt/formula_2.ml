06b2559b5d64a266ab98a3ba99d8ee4e32ed3411
bathan1 <side_nathan@hotmail.com>
2025-11-06 17:00:06 -0500

    Add dpll implementation for core SAT

--- src/smt/formula_2.ml
+++ src/smt/formula_2.ml
@@ -1,131 +1,217 @@
-let pos = Hashtbl.create (1 lsl 16) and neg = Hashtbl.create (1 lsl 16)
-let and2 (e_ls : (bool, 'k) Smt.Formula.t list) : (bool, 'k) Smt.Formula.t =
-    let stack = ref e_ls in
-    let rec loop acc =
-        match !stack with
-        | [] -> (match acc with
+module Solveable : Smt.Formula.SOLVABLE
+  with type ('a, 'k) t := ('a, 'k) Smt.Formula.t = struct
+    type ('a, 'k) t = ('a, 'k) Smt.Formula.t
+    let model_of_pairs
+        (pairs : ((bool, 'k) t * bool) list)
+        : 'k Smt.Model.t =
+        { value =
+            (fun (type a) (sym : (a, 'k) Smt.Symbol.t) ->
+                match sym with
+                | B _ ->
+                    let maybe_entry =
+                        Core.List.find pairs ~f:(fun (atom, _) ->
+                            match atom with
+                            | Smt.Formula.Key s -> Smt.Symbol.equal s sym
+                            | _ -> false)
+                    in
+                    (Core.Option.map maybe_entry ~f:snd : a option)
+                | I _ ->
+                    (None : a option))
+        }
+
+
+    let equal a b = Core.phys_equal a b || Core.Poly.equal a b
+    let const_int x = Smt.Formula.Const_int x
+    let const_bool x = Smt.Formula.Const_bool x
+    let symbol s = Smt.Formula.Key s
+    let and_ (type k) (e_ls : (bool, k) t list) : (bool, k) t =
+        let pos = Hashtbl.create (1 lsl 16) and neg = Hashtbl.create (1 lsl 16) in
+        let stack = ref e_ls in
+        let rec loop acc =
+            match !stack with
+            | [] -> (match acc with
                 | [] -> Smt.Formula.Const_bool true
                 | [e] -> e
                 | _ -> And (List.rev acc))
-        | Const_bool false :: _ -> Const_bool false
-        | Const_bool true :: tl -> stack := tl; loop acc
-        | And xs :: tl          -> stack := xs @ tl; loop acc
-        | (Not e as ne) :: tl   ->
-            stack := tl;
-            if Hashtbl.mem pos e then Const_bool false
-            else if Hashtbl.mem neg e then loop acc
-            else (Hashtbl.add neg e (); loop (ne :: acc))
-        | e :: tl ->
-            stack := tl;
-            if Hashtbl.mem neg e then Const_bool false
-            else if Hashtbl.mem pos e then loop acc
-            else (Hashtbl.add pos e(); loop (e :: acc))
-    in
-    loop []
+            | Const_bool false :: _ -> Const_bool false
+            | Const_bool true :: tl -> stack := tl; loop acc
+            | And xs :: tl          -> stack := xs @ tl; loop acc
+            | (Not e as ne) :: tl   ->
+                stack := tl;
+                if Hashtbl.mem pos e then Const_bool false
+                else if Hashtbl.mem neg e then loop acc
+                else (Hashtbl.add neg e (); loop (ne :: acc))
+            | e :: tl ->
+                stack := tl;
+                if Hashtbl.mem neg e then Const_bool false
+                else if Hashtbl.mem pos e then loop acc
+                else (Hashtbl.add pos e(); loop (e :: acc))
+        in
+        loop []
+    let rec not_ (e : (bool, 'k) t) : (bool, 'k) t =
+        match e with
+        | Const_bool b -> Const_bool (not b)
+        | Not e' -> e'
+        | Binop (Or, e1, e2) -> and_ [not_ e1 ; not_ e2]
+        | _ -> Not e
+    let is_const (type a) (x : (a, 'k) t) : bool =
+        match x with
+        | Const_int _ | Const_bool _ -> true
+        | _ -> false
+    let rec binop : type a b. (a * a * b) Smt.Binop.t -> (a, 'k) t -> (a, 'k) t -> (b, 'k) t =
+        fun op x y ->
+        match op with
+        | Or -> begin
+            match x, y with
+            | Const_bool true, _ -> Const_bool true
+            | _, Const_bool true -> Const_bool true
+            | Const_bool false, e -> e
+            | e, Const_bool false -> e
+            | e1, e2 -> Binop (Or, e1, e2)
+            end
+        | Equal -> begin
+            match x, y with
+            | Const_bool true, Key k -> Key k
+            | Key k, Const_bool true -> Key k
+            | Const_bool false, Key k -> Not (Key k)
+            | Key k, Const_bool false -> Not (Key k)
+            | Key k1, Key k2 when equal k1 k2 -> Const_bool true
+            | Const_bool b1, Const_bool b2 -> Const_bool (Bool.equal b1 b2)
+            | Const_int i1, Const_int i2 -> Const_bool (i1 = i2)
+            | e1, e2 -> Binop (Equal, e1, e2)
+            end
+        | Not_equal -> not_ (binop Equal x y)
+        | Plus -> begin
+            match x, y with
+            | e, Const_int 0
+                | Const_int 0, e -> e
+            | Const_int i1, Const_int i2 -> Const_int (i1 + i2)
+            | e1, e2 -> Binop (Plus, e1, e2)
+            end
+        | Minus -> begin
+            match x, y with
+            | e, Const_int 0 -> e
+            | Const_int i1, Const_int i2 -> Const_int (i1 - i2)
+            | e1, e2 -> Binop (Minus, e1, e2)
+            end
+        | Times -> begin
+            match x, y with
+            | e, Const_int 1
+                | Const_int 1, e -> e
+            | Const_int i1, Const_int i2 -> Const_int (i1 * i2)
+            | e1, e2 -> Binop (Times, e1, e2)
+            end
+        | Divide -> begin
+            match x, y with
+            | e, Const_int 1 -> e
+            | Const_int i1, Const_int i2 -> Const_int (i1 / i2)
+            | e1, e2 -> Binop (Divide, e1, e2)
+            end
+        | Modulus -> begin
+            match x, y with
+            | Const_int i1, Const_int i2 -> Const_int (i1 mod i2)
+            | e1, e2 -> Binop (Modulus, e1, e2)
+            end
+        | Less_than -> begin
+            match x, y with
+            | Const_int i1, Const_int i2 -> Const_bool (i1 < i2)
+            | e1, e2 -> Binop (Less_than, e1, e2)
+            end
+        | Less_than_eq -> begin
+            match x, y with
+            | Const_int i1, Const_int i2 -> Const_bool (i1 <= i2)
+            | e1, e2 -> Binop (Less_than_eq, e1, e2)
+            end
+        | Greater_than -> begin
+            match x, y with
+            | Const_int i1, Const_int i2 -> Const_bool (i1 > i2)
+            | e1, e2 -> Binop (Greater_than, e1, e2)
+            end
+        | Greater_than_eq -> begin
+            match x, y with
+            | Const_int i1, Const_int i2 -> Const_bool (i1 >= i2)
+            | e1, e2 -> Binop (Greater_than_eq, e1, e2)
+            end
 
-(* let or2 (e_ls : (bool, 'k) Smt.Formula.t list) : (bool, 'k) Smt.Formula.t = *)
-(*   let pos = Hashtbl.create (1 lsl 16) and neg = Hashtbl.create (1 lsl 16) in *)
-(*   let stack = ref e_ls in *)
-(*   let rec loop acc = *)
-(*     match !stack with *)
-(*     | [] -> (match acc with *)
-(*              | [] -> Smt.Formula.Const_bool false *)
-(*              | [e] -> e *)
-(*              | _ -> Binop (Or, Const_bool false, Const_bool false)) *)
-(*     | Const_bool true :: _ -> Const_bool true *)
-(*     | Const_bool false :: tl -> stack := tl; loop acc *)
-(*     | Binop (Or, e1, e2) :: tl -> stack := e1 :: e2 :: tl; loop acc *)
-(*     | (Not e as ne) :: tl -> *)
-(*         stack := tl; *)
-(*         if Hashtbl.mem neg e then Const_bool true *)
-(*         else if Hashtbl.mem pos e then loop acc *)
-(*         else (Hashtbl.add pos e (); loop (ne :: acc)) *)
-(*     | e :: tl -> *)
-(*         stack := tl; *)
-(*         if Hashtbl.mem pos e then Const_bool true *)
-(*         else if Hashtbl.mem neg e then loop acc *)
-(*         else (Hashtbl.add neg e (); loop (e :: acc)) *)
-(*   in *)
-(*   loop [] *)
+    open Smt.Formula
+    open Core
+    open Smt.Sat
 
-(* test begins here *)
-module Key : Smt.Symbol.KEY with type t = string = struct
-    type t = string
+    let solve (exprs : (bool, 'k) t list) : 'k Smt.Solution.t =
+        (** Convenience so we don't have to assert f' type for each Binop *)
+        let add_bool_binop acc (op : _ Smt.Binop.t) e1 e2 =
+            let f' : (bool, _) t = Binop (op, e1, e2) in
+            f' :: acc
+        in
+        let rec collect_atoms : type a k. (bool, k) t list -> (a, k) t -> (bool, k) t list =
+            fun acc f -> match f with
+                | Const_bool _ | Const_int _ -> acc
+                | Key (B _) -> f :: acc
+                | Key (I _) -> acc
+                | Not e -> collect_atoms acc e
+                | And es -> List.fold_left es ~init:acc ~f:collect_atoms
+                | Binop (Equal, e1, e2) ->
+                    add_bool_binop acc Equal e1 e2
+                | Binop (Less_than, e1, e2) ->
+                    add_bool_binop acc Less_than e1 e2
+                | Binop (Greater_than, e1, e2) ->
+                    add_bool_binop acc Greater_than e1 e2
+                | Binop (Less_than_eq, e1, e2) ->
+                    add_bool_binop acc Less_than_eq e1 e2
+                | Binop (Greater_than_eq, e1, e2) ->
+                    add_bool_binop acc Greater_than_eq e1 e2
+                | Binop (_, e1, e2) ->
+                    let acc = collect_atoms acc e1 in
+                    collect_atoms acc e2
+        in
+        let atoms =
+            exprs
+            |> List.concat_map ~f:(collect_atoms [])
+            |> List.dedup_and_sort ~compare:Poly.compare
+        in
+        let atom_table = Hashtbl.Poly.create () 
+        and id_table = Hashtbl.create (module Int)
+        and counter = ref 0 in
+        List.iter atoms ~f:(fun atom ->
+            Hashtbl.set atom_table ~key:atom ~data:!counter;
+            Hashtbl.set id_table ~key:!counter ~data:atom;
+            incr counter);
 
-    let uid (t : t) : int =
-        Hashtbl.hash t
+        let rec to_cnf f : int cnf =
+            match f with
+            | Const_bool true -> []
+            | Const_bool false -> [ [] ]
+            | Key _ | Binop ((Equal | Less_than | Greater_than
+                | Less_than_eq | Greater_than_eq), _, _) ->
+                let id = Hashtbl.find_exn atom_table f in
+                [ [Pos id] ]
+            | Not e -> (
+                match e with
+                | Key _ | Binop ((Equal | Less_than | Greater_than
+                    | Less_than_eq | Greater_than_eq), _, _) ->
+                    let id = Hashtbl.find_exn atom_table e in
+                    [ [Neg id] ]
+                | _ ->
+                    to_cnf (not_ e) )
+            | And es -> List.concat_map es ~f:to_cnf
+            | Binop (Or, e1, e2) ->
+                let c1, c2 = to_cnf e1, to_cnf e2 in
+                List.concat_map c1 ~f:(fun a ->
+                    List.map c2 ~f:(fun b -> a @ b))
+            | _ -> []
+        in
+        let cnf = List.concat_map exprs ~f:to_cnf in
+
+        match dpll Int.equal [] cnf with
+        | None -> Smt.Solution.Unsat
+        | Some assignment ->
+            let model_pairs =
+                List.filter_map assignment ~f:(fun (id, value) ->
+                    match Hashtbl.find id_table id with
+                    | Some atom -> Some (atom, value)
+                    | None -> None)
+            in
+            let model = model_of_pairs model_pairs in
+            Smt.Solution.Sat model
 end
-
-module Symbol_string = Smt.Symbol.Make(Key)
-
-open Core
-
-let bench ~runs label (f : unit -> 'a) =
-  ignore (f ());
-
-  let total = ref Time_ns.Span.zero in
-  let min_  = ref Time_ns.Span.max_value_representable in
-  let max_  = ref Time_ns.Span.min_value_representable in
-
-  for _ = 1 to runs do
-    let t0 = Time_ns.now () in
-    ignore (Sys.opaque_identity (f ()));
-    let dt = Time_ns.diff (Time_ns.now ()) t0 in
-    total := Time_ns.Span.( !total + dt );
-    if Time_ns.Span.( dt < !min_ ) then min_ := dt;
-    if Time_ns.Span.( dt > !max_ ) then max_ := dt;
-  done;
-
-  let total_ns =
-    Time_ns.Span.to_int63_ns !total |> Int63.to_float
-  in
-  let min_ns =
-    Time_ns.Span.to_int63_ns !min_  |> Int63.to_float
-  in
-  let max_ns =
-    Time_ns.Span.to_int63_ns !max_  |> Int63.to_float
-  in
-  let avg_ns = total_ns /. float runs in
-
-  printf "%-6s runs=%d  avg=%.3f µs  min=%.3f µs  max=%.3f µs\n"
-    label runs (avg_ns /. 1e3) (min_ns /. 1e3) (max_ns /. 1e3)
-
-let key s : (bool, string) Smt.Formula.t = Smt.Formula.Key (Symbol_string.make_bool s)
-
-let mk_tail n : (bool, string) Smt.Formula.t list =
-  List.init n ~f:(fun i -> key (Int.to_string i))
-
-let front_contradiction n : (bool, string) Smt.Formula.t list =
-  key "x" :: Not (key "x") :: mk_tail n
-
-let end_contradiction n : (bool, string) Smt.Formula.t list =
-  key "x" :: mk_tail n @ [ Not (key "x") ]
-
-let no_contradiction n : (bool, string) Smt.Formula.t list =
-  key "x" :: mk_tail n
-
-let () =
-    (* (not (b = a)) ^ (d = c) *)
-    (* let a = key "a" in *)
-    (* let b = key "b" in *)
-    (* let c = key "c" in *)
-    (* let d = key "d" in *)
-    (* let expr = [ *)
-    (*     Smt.Formula.Not (Binop (Equal, b, a)); *)
-    (*     Binop (Equal, d, c) *)
-    (* ] in *)
-    (* let result = and2 expr in *)
-    let n = 100 in
-    let f_front = front_contradiction n in
-    let f_end   = end_contradiction   n in
-    let f_none  = no_contradiction    n in
-
-    bench ~runs:10 "and_  front" (fun () -> Smt.Formula.and_ f_front);
-    bench ~runs:10 "and2  front" (fun () -> and2 f_front);
-
-    bench ~runs:5  "and_  end  " (fun () -> Smt.Formula.and_ f_end);
-    bench ~runs:5  "and2  end  " (fun () -> and2 f_end);
-
-    bench ~runs:3  "and_  none " (fun () -> Smt.Formula.and_ f_none);
-    bench ~runs:3  "and2  none " (fun () -> and2 f_none)
 
