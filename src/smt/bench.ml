cec06dc5030bdb5871b4690bcc0bbe395cc803d0
bathan1 <side_nathan@hotmail.com>
2025-11-11 11:14:48 -0500

    Add assignment simplifier

--- src/smt/bench.ml
+++ src/smt/bench.ml
@@ -1,59 +1,42 @@
 open Formula_2
 
-open Core
+let hashtbl_to_string
+    (tbl : ('k, 'v) Hashtbl.t)
+    ~(string_of_key : 'k -> string)
+    ~(string_of_val : 'v -> string)
+    : string =
+    let buf = Buffer.create 64 in
+    Buffer.add_string buf "{";
+    let first = ref true in
+    Hashtbl.iter
+        (fun k v ->
+            if !first then first := false else Buffer.add_string buf "; ";
+            Buffer.add_string buf (Printf.sprintf "%s -> %s"
+                (string_of_key k)
+                (string_of_val v)))
+        tbl;
+    Buffer.add_string buf "}";
+    Buffer.contents buf
 
 let () =
     try
-        let a = AsciiSymbol.make_int "a" 
-        and b = AsciiSymbol.make_int "b" and c = AsciiSymbol.make_int "c" in
-        let expected_sats = [
-            [Smt.Formula.And [
-                Binop (Equal, Key a, Key b);
-                Binop (Equal, Key b, Key c);
-            ]];
-            [Smt.Formula.And [
-                Binop (Equal, Key a, Key b);
-                Binop (Equal, Key b, Const_int 123456);
-            ]];
-            [Smt.Formula.And [
-                Binop (Equal, Key a, Key b);
-            ]]
+        let a = AsciiSymbol.make_int "a" and b = AsciiSymbol.make_int "b" in
+        let expr = [ 
+            Smt.Formula.And [
+                Binop (Equal, Key a, Const_int 123);
+                Binop (Greater_than, Key b, Key a)
+            ]
         ] in
-        List.iter expected_sats ~f:(fun expr -> ignore (solve expr));
-        let expected_unsats = [
-            [Smt.Formula.Const_bool false];
-            [Smt.Formula.And [
-                Smt.Formula.Const_bool true;
-                Smt.Formula.Const_bool false;
-            ]];
-            [Smt.Formula.And [
-                Binop (Not_equal, Key a, Key a)
-            ]];
-            [Smt.Formula.And [
-                Binop (Equal, Key a, Key b);
-                Binop (Equal, Key b, Key c);
-                Binop (Not_equal, Key a, Key c)
-            ]];
-        ] in
-        List.iter expected_unsats ~f:(fun expr -> ignore (solve expr));
-        (* In_channel.read_all "formulas_dedup_2.txt" *)
-        (* |> of_string *)
-        (* |> List.iter ~f:(fun expr -> ignore (solve [expr])); *)
-        (* let impossible_expr = Smt.Formula.And [ *)
-        (*     Not (Binop (Equal, Key a, Key a)) *)
-        (* ] in *)
-        (* let impossible_expr2 = Smt.Formula.And [ *)
-        (*     Binop (Not_equal, Key a, Key a) *)
-        (* ] in *)
-        (* let impossible_expr3 = Smt.Formula.And [ *)
-        (*     Binop (Equal, Key a, Key b); *)
-        (*     Binop (Equal, Key b, Key c); *)
-        (* ] in *)
-        (* ignore (solve [impossible_expr]); *)
-        (* ignore (solve [impossible_expr2]); *)
-        (* ignore (solve [impossible_expr3]); *)
+        let hash, simplified = simplify expr in
+        Printf.printf "hash is %s\n" (hashtbl_to_string hash
+            ~string_of_key:(fun (key, _) -> key |> Char.chr |> Core.Char.to_string)
+            ~string_of_val:Int.to_string
+        );
+        Printf.printf "simplified is %s\n" (
+            List.fold_left (fun acc expr -> acc ^ (to_string expr)) "" simplified
+        );
     with
         | e ->
-        eprintf "Error: %s\n" (Exn.to_string e);
+        Printf.eprintf "Error: %s\n" (Core.Exn.to_string e);
         exit 1
 
