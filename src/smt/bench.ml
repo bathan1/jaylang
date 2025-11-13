cf9de17da58d5d3c64f4bb1584e576f56f2a5b27
bathan1 <side_nathan@hotmail.com>
2025-11-13 12:09:52 -0500

    Move simplify into formula

--- src/smt/bench.ml
+++ src/smt/bench.ml
@@ -1,49 +1,30 @@
-open Formula_2
-
-let intmap_to_string
-    (tbl : lia_cst_t IntMap.t)
-    ~(string_of_key : int -> string)
-    : string =
-    let buf = Buffer.create 128 in
-    Buffer.add_string buf "{";
-    let first = ref true in
-    IntMap.iter
-        (fun k v ->
-            if !first then first := false else Buffer.add_string buf "; ";
-            let lower_str =
-                match v.lower with
-                | Some l -> Printf.sprintf "lower=%d" l
-                | None -> "lower=_"
-            in
-            let upper_str =
-                match v.upper with
-                | Some u -> Printf.sprintf "upper=%d" u
-                | None -> "upper=_"
-            in
-            let neq_str =
-                if v.neq = [] then "neq=[]"
-                else
-                    let inner =
-                        v.neq |> List.map string_of_int |> String.concat ","
-                    in
-                    Printf.sprintf "neq=[%s]" inner
-            in
-            Buffer.add_string buf
-                (Printf.sprintf "%s -> {%s; %s; %s}"
-                    (string_of_key k)
-                    lower_str upper_str neq_str))
-        tbl;
-    Buffer.add_string buf "}";
-    Buffer.contents buf
-
-open Core
 open Smt.Formula
+open Core
 
-let a = AsciiSymbol.make_int "a" 
-and b = AsciiSymbol.make_int "b"
-and c = AsciiSymbol.make_int "c"
-and d = AsciiSymbol.make_int "d"
-and e = AsciiSymbol.make_int "e";;
+let lia_table_to_string tbl ~string_of_key =
+  let entries =
+    Map.to_alist tbl
+    |> List.map ~f:(fun (key, data) ->
+         let lower =
+           Option.value_map data.lower ~default:"_" ~f:Int.to_string
+         and upper =
+           Option.value_map data.upper ~default:"_" ~f:Int.to_string
+         and neq =
+           data.neq |> List.map ~f:Int.to_string |> String.concat ~sep:","
+         in
+         sprintf "  %s -> {lower=%s; upper=%s; neq=[%s]}"
+           (string_of_key key) lower upper neq)
+    |> String.concat ~sep:"\n"
+  in
+  sprintf "LIA table:\n{\n%s\n}" entries
+
+open Smt.Formula_2
+(* Example symbols *)
+let a = AsciiSymbol.make_int "a"
+let b = AsciiSymbol.make_int "b"
+let c = AsciiSymbol.make_int "c"
+let d = AsciiSymbol.make_int "d"
+let e = AsciiSymbol.make_int "e"
 
 let a_eq_123456 = Binop (Equal, Key a, Const_int 123456)
 let b_eq_a = Binop (Equal, Key a, Key b)
@@ -56,91 +37,92 @@ let d_eq_123456 = Binop (Equal, Key d, Const_int 123456)
 let e_eq_123456 = Binop (Equal, Key e, Const_int 123456)
 
 let formulas = [
-    [ a_eq_123456 ];
-    [ Not b_eq_a ];
-    [
-        And [
-            Not b_eq_a;
-            d_eq_c;
-        ]
-    ];
-    [
-        And [
-            a_eq_123456;
-            b_eq_123456;
-        ]
-    ];
-    [
-        And [
-            a_eq_123456;
-            Not b_eq_123456;
-            c_eq_123456;
-        ]
-    ];
-    [
-        And [
-            a_eq_123456;
-            b_eq_123456;
-            c_eq_123456;
-        ]
-    ];
-    [
-        And [
-            b_lt_a;
-            Not c_eq_123456;
-            Not d_eq_123456;
-            e_eq_123456;
-        ]
-    ];
-    [
-        And [
-            a_eq_123456;
-            Not b_eq_123456;
-            c_eq_123456;
-            d_eq_123456;
-        ]
-    ];
-    [
-        And [
-            a_eq_123456;
-            b_eq_123456;
-            Not c_eq_123456;
-            d_eq_123456;
-        ]
-    ];
-    [
-        And [
-            a_eq_123456;
-            b_eq_123456;
-            c_eq_123456;
-            d_eq_123456;
-        ]
-    ];
-    [
-        And [
-            Not b_gt_a;
-            Not b_lt_a;
-            Not c_eq_123456;
-            Not d_eq_123456;
-            Not e_eq_123456;
-        ]
-    ];
+  [ a_eq_123456 ];
+  [ Not b_eq_a ];
+  [
+    And [
+      Not b_eq_a;
+      d_eq_c;
+    ]
+  ];
+  [
+    And [
+      a_eq_123456;
+      b_eq_123456;
+    ]
+  ];
+  [
+    And [
+      a_eq_123456;
+      Not b_eq_123456;
+      c_eq_123456;
+    ]
+  ];
+  [
+    And [
+      a_eq_123456;
+      b_eq_123456;
+      c_eq_123456;
+    ]
+  ];
+  [
+    And [
+      b_lt_a;
+      Not c_eq_123456;
+      Not d_eq_123456;
+      e_eq_123456;
+    ]
+  ];
+  [
+    And [
+      a_eq_123456;
+      Not b_eq_123456;
+      c_eq_123456;
+      d_eq_123456;
+    ]
+  ];
+  [
+    And [
+      a_eq_123456;
+      b_eq_123456;
+      Not c_eq_123456;
+      d_eq_123456;
+    ]
+  ];
+  [
+    And [
+      a_eq_123456;
+      b_eq_123456;
+      c_eq_123456;
+      d_eq_123456;
+    ]
+  ];
+  [
+    And [
+      Not b_gt_a;
+      Not b_lt_a;
+      Not c_eq_123456;
+      Not d_eq_123456;
+      Not e_eq_123456;
+    ]
+  ];
 ]
 
 let () =
-    List.iteri formulas ~f:(fun i fml ->
-        printf "======= Formula %d =======\n" (i + 1);
-        let start_ns = Time_ns.now () in
-        let tbl, simplified = simplify fml in
-        let stop_ns = Time_ns.now () in
-        let elapsed_ns = Time_ns.diff stop_ns start_ns |> Time_ns.Span.to_int_ns in
+  let open Smt.Formula in
+  List.iteri formulas ~f:(fun i fml ->
+    printf "======= Formula %d =======\n" (i + 1);
+    let start_ns = Time_ns.now () in
+    let tbl, simplified = simplify fml in
+    let stop_ns = Time_ns.now () in
+    let elapsed_ns = Time_ns.diff stop_ns start_ns |> Time_ns.Span.to_int_ns in
 
-        printf "Time: %d ns\n" elapsed_ns;
-        printf "LIA table: %s\n"
-            (intmap_to_string tbl
-                ~string_of_key:(fun k -> Char.of_int_exn k |> Char.to_string));
-        printf "Original: %s\n" (List.map fml ~f:to_string |> String.concat ~sep:" ^ ");
-        printf "Simplified: %s\n\n"
-            (List.map simplified ~f:to_string |> String.concat ~sep:" ^ ");
-    )
+    printf "Time: %d ns\n" elapsed_ns;
+    printf "LIA table: %s\n"
+      (lia_table_to_string tbl
+        ~string_of_key:(fun k -> Char.of_int_exn k |> Char.to_string));
+    printf "Original: %s\n" (List.map fml ~f:to_string |> String.concat ~sep:" ^ ");
+    printf "Simplified: %s\n\n"
+      (List.map simplified ~f:to_string |> String.concat ~sep:" ^ ");
+  )
 ;;
--- src/smt/formula_2.ml
+++ src/smt/formula_2.ml
@@ -1,7 +1,7 @@
-open Smt.Sat
+open Sat
 
-(** Use same type as Smt.Formula.t *)
-type ('a, 'k) t = ('a, 'k) Smt.Formula.t
+(** Use same type as Formula.t *)
+type ('a, 'k) t = ('a, 'k) Formula.t
 
 let rec to_string : type a k. (a, k) t -> string = fun expr ->
     let open Core in
@@ -38,190 +38,7 @@ let rec to_string : type a k. (a, k) t -> string = fun expr ->
         in
         Printf.sprintf "(%s %s %s)" (to_string e1) op_str (to_string e2)
 
-type lia_cst_t = {
-    neq : int list;
-    lower : int option;
-    upper : int option;
-}
-
-let empty_lia_cst : lia_cst_t = {
-    neq = [];
-    lower = None;
-    upper = None;
-}
-
-module IntMap = Map.Make (Int)
-
-let rec next :
-    type k.
-    lia_cst_t IntMap.t ->
-    (bool, k) t ->
-    lia_cst_t IntMap.t
-    = fun acc el ->
-    match el with
-    | Formula.Binop (op, Const_int v, Key (I x)) ->
-        let curr =
-            match IntMap.find_opt x acc with
-            | Some bounds -> bounds
-            | None -> empty_lia_cst
-        in
-        let cst =
-            match op with
-            | Equal -> { curr with lower = Some v; upper = Some (v + 1) }
-            | Not_equal -> { curr with neq = v :: curr.neq }
-            | Greater_than -> { curr with lower = Some (v + 1) }
-            | Greater_than_eq -> { curr with lower = Some v }
-            | Less_than -> { curr with upper = Some v }
-            | Less_than_eq -> { curr with upper = Some (v + 1) }
-        in
-        IntMap.add x cst acc
-    | Formula.Binop (op, Key (I x), Const_int v) ->
-        let curr =
-            match IntMap.find_opt x acc with
-            | Some bounds -> bounds
-            | None -> empty_lia_cst
-        in
-        let cst =
-            match op with
-            | Equal -> { curr with lower = Some v; upper = Some (v + 1) }
-            | Not_equal -> { curr with neq = v :: curr.neq }
-            | Greater_than -> { curr with lower = Some (v + 1) }
-            | Greater_than_eq -> { curr with lower = Some v }
-            | Less_than -> { curr with upper = Some v }
-            | Less_than_eq -> { curr with upper = Some (v + 1) }
-        in
-        IntMap.add x cst acc
-    | Formula.Binop (_, Key (I x), Key (I y)) ->
-        let acc =
-            if IntMap.mem x acc then acc else IntMap.add x empty_lia_cst acc
-        in
-        if IntMap.mem y acc then acc else IntMap.add y empty_lia_cst acc
-
-    | Formula.Key key ->
-        let x = Utils.Separate.extract key in
-        if IntMap.mem x acc then acc else IntMap.add x empty_lia_cst acc
-
-    | Formula.And ls ->
-        List.fold_left next acc ls
-
-    | _ -> acc
-;;
-
-let decide_eq :
-    type k.
-    (bool, k) t ->
-    lia_cst_t ->
-    lia_cst_t ->
-    (bool, k) t
-    = fun expr lcst rcst ->
-    let open Smt.Formula in
-    match (lcst.lower, lcst.upper, rcst.lower, rcst.upper) with
-    | (Some xl, Some xu, Some yl, Some yu) when xl = xu && yl = yu ->
-        Const_bool (xl = yl)
-    | (Some xl, Some xu, Some yl, Some yu) when xu < yl || yu < xl ->
-        Const_bool false
-    | (Some xl, Some xu, _, _) when xl = xu && List.mem xl rcst.neq ->
-        Const_bool false
-    | (_, _, Some yl, Some yu) when yl = yu && List.mem yl rcst.neq ->
-        Const_bool false
-    | _ -> expr;;
-
-
-let decide_neq :
-    type k.
-    (bool, k) t ->
-    lia_cst_t ->
-    lia_cst_t ->
-    (bool, k) t
-    = fun expr lcst rcst ->
-    match decide_eq expr lcst rcst with
-    | Const_bool true  -> Const_bool false
-    | Const_bool false -> Const_bool true
-    | _ -> expr
-;;
-
-let decide_gt :
-    type k.
-    int ->
-    int ->
-    lia_cst_t ->
-    lia_cst_t ->
-    (bool, k) t
-    = fun x y xcst ycst ->
-    match xcst.lower, xcst.upper, ycst.lower, ycst.upper with
-    | Some xl, _, _, Some yu when xl >= yu ->
-        Const_bool true
-    | _, Some xu, Some yl, _ when xu <= yl ->
-        Const_bool false
-    | (Some xl, Some xu, Some yl, Some yu)
-        when xu <= yl || yu <= xl ->
-        Const_bool (xl > yu)
-    | (Some xl, Some xu, None, None)
-        when xu - xl = 1 ->
-        Binop (Less_than_eq, Key (I y), Const_int xl)
-    | (None, None, Some yl, Some yu)
-        when yu - yl = 1 ->
-        Binop (Greater_than, Key (I x), Const_int yl)
-    | _ -> Binop (Greater_than, Key (I x), Key (I y))
-
-
-let rec sub :
-    type k.
-    lia_cst_t IntMap.t ->
-    (bool, k) t ->
-    (bool, k) t option
-    = fun acc expr ->
-    match expr with
-    | Binop (_, Key (I _), Const_int _)
-        | Binop (_, Const_int _, Key (I _)) -> None
-    | Binop (op, Key (I x), Key (I y)) ->
-        let x_cst = IntMap.find x acc
-        and y_cst = IntMap.find y acc
-        in
-        let decided_expr = 
-            (match op with
-                | Equal -> decide_eq expr x_cst y_cst
-                | Not_equal -> decide_neq expr x_cst y_cst
-                | Less_than_eq
-                    | Greater_than -> decide_gt x y x_cst y_cst
-                | Greater_than_eq
-                    | Less_than -> decide_gt x y y_cst x_cst)
-        in
-        Some decided_expr
-    | And rest ->
-        let simplified_subs =
-            rest
-            |> List.filter_map (sub acc)
-        in
-        let contains_false =
-            List.exists (function Smt.Formula.Const_bool false -> true | _ -> false) simplified_subs
-        in
-        if contains_false then Some (Const_bool false)
-        else
-            let non_trivial =
-                List.filter (function Smt.Formula.Const_bool true -> false | _ -> true) simplified_subs
-            in
-            begin match non_trivial with
-                | [] -> Some (Const_bool true)
-                | [single] -> Some single
-                | ls -> Some (And ls)
-                end
-    | _ -> Some expr
-;;
-
-let simplify
-    : type k.
-    (bool, k) t list ->
-    lia_cst_t IntMap.t * (bool, k) t list
-    = fun exprs ->
-    exprs
-    |> List.fold_left next (IntMap.empty : lia_cst_t IntMap.t)
-    |> fun tbl ->
-    List.filter_map (fun el -> sub tbl el) exprs
-    |> fun red -> (tbl, red)
-;;
-
-let rec tseitin (f : ('a, 'k) Smt.Formula.t)
+let rec tseitin (f : ('a, 'k) Formula.t)
     (fresh : ('a, 'k) t -> int)
     (cnf_acc : int cnf)
     (atom_table : (('a, 'k) t, int) Core.Hashtbl.t)
@@ -286,15 +103,15 @@ let rec tseitin (f : ('a, 'k) Smt.Formula.t)
 
 let model_of_pairs
     (pairs : ((bool, 'k) t * bool) list)
-    : 'k Smt.Model.t =
+    : 'k Model.t =
     { value =
-        (fun (type a) (sym : (a, 'k) Smt.Symbol.t) ->
+        (fun (type a) (sym : (a, 'k) Symbol.t) ->
             match sym with
             | B _ ->
                 let maybe_entry =
                     Core.List.find pairs ~f:(fun (atom, _) ->
                         match atom with
-                        | Smt.Formula.Key s -> Smt.Symbol.equal s sym
+                        | Formula.Key s -> Symbol.equal s sym
                         | _ -> false)
                 in
                 (Core.Option.map maybe_entry ~f:snd : a option)
@@ -302,17 +119,17 @@ let model_of_pairs
                 (None : a option))
     }
 
-let equal = Smt.Formula.equal
-let const_int = Smt.Formula.const_int
-let const_bool = Smt.Formula.const_bool
-let symbol = Smt.Formula.symbol
+let equal = Formula.equal
+let const_int = Formula.const_int
+let const_bool = Formula.const_bool
+let symbol = Formula.symbol
 let and_ (type k) (e_ls : (bool, k) t list) : (bool, k) t =
     let pos = Hashtbl.create (1 lsl 16) and neg = Hashtbl.create (1 lsl 16) in
     let stack = ref e_ls in
     let rec loop acc =
         match !stack with
         | [] -> (match acc with
-            | [] -> Smt.Formula.Const_bool true
+            | [] -> Formula.Const_bool true
             | [e] -> e
             | _ -> And (List.rev acc))
         | Const_bool false :: _ -> Const_bool false
@@ -337,9 +154,9 @@ let rec not_ (e : (bool, 'k) t) : (bool, 'k) t =
     | Binop (Or, e1, e2) -> and_ [not_ e1 ; not_ e2]
     | _ -> Not e
 
-let is_const = Smt.Formula.is_const
+let is_const = Formula.is_const
 
-let rec binop : type a b. (a * a * b) Smt.Binop.t -> (a, 'k) t -> (a, 'k) t -> (b, 'k) t =
+let rec binop : type a b. (a * a * b) Binop.t -> (a, 'k) t -> (a, 'k) t -> (b, 'k) t =
     fun op x y ->
     match op with
     | Or -> begin
@@ -435,18 +252,13 @@ let rec collect_atoms : type a k. (bool, k) t list -> (a, k) t -> (bool, k) t li
 ;;
 
 
-let solve (exprs : (bool, 'k) t list) : 'k Smt.Solution.t =
+let solve (exprs : (bool, 'k) t list) : 'k Solution.t =
     let open Core in
-    let open Smt.Formula in
+    let open Formula in
 
     let atom_table = Hashtbl.Poly.create ()
     and id_table = Hashtbl.create (module Int)
     and counter = ref 0 in
-
-    (* Printf.printf "\027[1;34m======= SMT solve() run =======\027[0m\n"; *)
-    (* List.iteri exprs ~f:(fun i e -> *)
-    (*     Printf.printf "Expr %d: %s\n" (i + 1) (to_string e)); *)
-    (* print_endline "-----------------------------------"; *)
 
     let atoms =
         exprs
@@ -458,10 +270,6 @@ let solve (exprs : (bool, 'k) t list) : 'k Smt.Solution.t =
         Hashtbl.set atom_table ~key:atom ~data:!counter;
         Hashtbl.set id_table ~key:!counter ~data:atom;
         incr counter);
-
-    (* Printf.printf "\027[1;33mAtom table:\027[0m\n"; *)
-    (* Hashtbl.iteri atom_table ~f:(fun ~key:atom ~data:id -> *)
-    (*     Printf.printf "  %3d <-> %s\n" id (to_string atom)); *)
 
     let fresh data =
         let v = !counter in
@@ -487,35 +295,10 @@ let solve (exprs : (bool, 'k) t list) : 'k Smt.Solution.t =
             if is_neg e then [Neg v] else [Pos v])
     in
     let cnf = top_clauses @ cnf in
-
-    (* Printf.printf "CNF generated: %d clauses, %d vars\n" *)
-    (*     (List.length cnf) !counter; *)
-    (* Printf.printf "CNF:\n"; *)
-    (* List.iteri cnf ~f:(fun i clause -> *)
-    (*     Printf.printf "  %d: %s\n" i *)
-    (*         (String.concat ~sep:" ∨ " *)
-    (*             (List.map clause ~f:(function *)
-    (*                 | Pos x -> Printf.sprintf "v%d" x *)
-    (*                 | Neg x -> Printf.sprintf "¬v%d" x)))); *)
-    (* print_endline "-----------------------------------"; *)
-
     match dpll equal [] cnf with
     | None ->
-        (* print_endline "\027[1;31mSAT Result: UNSAT\027[0m"; *)
-        (* print_endline "-----------------------------------\n"; *)
-        Smt.Solution.Unsat
+        Solution.Unsat
     | Some assignment ->
-        (* print_endline "\027[1;32mSAT Result: SAT\027[0m"; *)
-        (* Printf.printf "Raw assignment (%d vars):\n" (List.length assignment); *)
-        (* List.iter assignment ~f:(fun (id, value) -> *)
-        (*     Printf.printf "  v%-3d = %B\n" id value); *)
-        (* print_endline "-----------------------------------"; *)
-
-        (* Printf.printf "ID table:\n"; *)
-        (* Hashtbl.iteri id_table ~f:(fun ~key ~data -> *)
-        (*     if Hashtbl.mem atom_table data then *)
-        (*         Printf.printf "  %d -> %s\n" key (to_string data)); *)
-
         let model_pairs =
             List.filter_map assignment ~f:(fun (id, value) ->
                 Option.map (Hashtbl.find id_table id) ~f:(fun atom ->
@@ -524,15 +307,10 @@ let solve (exprs : (bool, 'k) t list) : 'k Smt.Solution.t =
         in
 
         if not (Theory.check model_pairs) then (
-            (* print_endline *)
-            (*     "\027[1;31mTheory conflict — UNSAT after theory check.\027[0m"; *)
-            (* print_endline "-----------------------------------\n"; *)
             Solution.Unsat)
         else
             match Theory.model model_pairs with
             | None ->
-                (* print_endline "\027[1;31mTheory found no valid model.\027[0m"; *)
-                (* print_endline "-----------------------------------\n"; *)
                 Solution.Unsat
             | Some int_model ->
                 let constraints = Theory.extract_constraints model_pairs in
@@ -545,11 +323,6 @@ let solve (exprs : (bool, 'k) t list) : 'k Smt.Solution.t =
                                 | Some v -> Hashtbl.set int_model ~key:root ~data:v
                                 | None -> Hashtbl.set int_model ~key:x ~data:0)));
 
-                (* print_endline "\027[1;36mCombined theory model:\027[0m"; *)
-                (* Hashtbl.iteri int_model ~f:(fun ~key ~data -> *)
-                (*     if key >= 97 && key <= 122 then *)
-                (*         Printf.printf "  %c = %d\n" (Char.of_int_exn key) data); *)
-                (* print_endline "-----------------------------------\n"; *)
 
                 let model = model_of_pairs model_pairs in
                 Solution.Sat model
