fec9a0818a1b367dc3d751ae4dde157c174a9e6d
bathan1 <side_nathan@hotmail.com>
2025-11-20 16:50:33 -0500

    Add try-count config var to lucky

--- src/smt/lucky.ml
+++ src/smt/lucky.ml
@@ -1,7 +1,14 @@
 (* Nathan's Lucky Guess algorithm implementation.
    Courtesy of Dr. Scott Smith.
+
    ...
 
-   Given some formula F, with variables
-   x1, x2, ..., xn
+   Given some conjunctive formula F, with variables
+   x1, x2, ..., xn. [Lucky] will attempt to make a
+   guess on a SAT assignment
    *)
+
+(** Static config variable that sets the max number of 
+    attempts {!Lucky.solve} can make.
+*)
+let __TRY_COUNT__ = 1
